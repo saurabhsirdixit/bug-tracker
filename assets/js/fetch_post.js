f4dc99f58fb7d62b371585ff935d01781de6db2f
Ashish Kumar <akmashish15@gmail.com>
2017-11-27 19:25:35 +0530

    post type projcts with ajax

---
+++ assets/js/fetch_post.js
@@ -0,0 +1,42 @@
+ $(document).ready(function() {
+
+        posts = 8;
+        author_posts = parseInt(<?php echo $author_posts; ?>);
+
+        $("#link_selector").click(function() {
+
+            // alert('posts - ' + posts + 'author posts - ' + author_posts);
+
+
+            if ((posts - author_posts) > 3) {
+                $("#link_selector").text('No more posts!');
+            }
+            else {
+
+                var category        = '<?php echo strtolower($category); ?>';
+                var id              = '<?php echo $blogger_id; ?>';
+                var firstname       = '<?php echo $firstname; ?>';
+                var surname         = '<?php echo $surname; ?>';
+
+
+                // alert(posts + category + id + firstname + surname);
+
+                $.ajax({
+                    type: "GET",
+                    url: "/wordpress/wp-admin/admin-ajax.php",
+                    dataType: 'html',
+                    data: ({ action: 'loadMore', id: id, firstname: firstname, surname: surname, posts: posts, category: category}),
+                    success: function(data){
+                        $('#ajax_results_container').hide().fadeIn('slow').html(data);
+                        posts = posts + 4;
+
+                        if ((posts - author_posts) > 3) {
+                            $("#link_selector").text('No more posts!');
+                        }
+
+                    }
+                });
+            }
+
+        });
+    });
